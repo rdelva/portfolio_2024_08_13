bf7a450e33b66899e609add5c3d149d62c767cd7
rdelva <roseldelva@gmail.com>
2024-08-15 21:55:35 -0400

    added about me route and contact me route

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());    
 
-app.get('/', (request, response)=> {
-    response.send('I love treehouse');
+app.get('/', (req, res)=> {
+    res.send('I love treehouse');
 });
 
-app.get('/', (request, response)=> {
-    response.send('I love treehouse');
+app.get('/aboutme', (req, res)=> {
+    res.send('About Me');
 });
 
+
+app.get('/contact me', (req, res)=> {
+    res.send('Contact Me');
+});
+
+
+//Sets up the devlopment server which will run on my machine
 app.listen(3000, () =>{
     console.log('The application is running on localhost:3000');
 });
